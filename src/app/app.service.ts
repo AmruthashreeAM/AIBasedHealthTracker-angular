2cba587b34496980ffa3b01dd0653e75819f860f
AmruthashreeAM <>
2021-08-14 22:41:54 -0500

    Initial commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,8 +12,12 @@ export class AppService {
 
   constructor(public httpClient: HttpClient) { }
 
-  getDeals(username): Observable<any> {
+  getTemperature(username): Observable<any> {
     return this.httpClient.get('https://healthcare-server123.herokuapp.com/temperature/'+username);
+  }
+
+  getPill(username): Observable<any> {
+    return this.httpClient.get('https://healthcare-server123.herokuapp.com//pill/'+username);
   }
 
   updateTemperature(username,temparature): any { 
@@ -22,11 +26,19 @@ export class AppService {
     return this.httpClient.post('https://healthcare-server123.herokuapp.com/temperature/' + username + '/updateTemperature/',temparature);
   }
 
-  generateRecords(username): Observable<any> { 
-    return this.httpClient.get('https://healthcare-server123.herokuapp.com/generateRecords');
-  }
+  updatePillData(username,pillTaken): any { 
+    // const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+ 
+     return this.httpClient.post('https://healthcare-server123.herokuapp.com/pill/' + username + '/updatePill/',pillTaken);
+   }  
 
+  generateRecords(username): Observable<any> { 
+    return this.httpClient.get('http://localhost:5000/generateRecords');
+  }
+  generatePillData(username): Observable<any> { 
+    return this.httpClient.get('http://localhost:5002/generatePillData');
+  }
   generateIndentifiedPateints(username): Observable<any> { 
-    return this.httpClient.get('https://healthcare-server123.herokuapp.com/facialRecognition');
+    return this.httpClient.get('http://localhost:5001/facialRecognition');
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,6 +23,8 @@ export class HomeComponent {
   cardsForHandset = [];
   ids = [];
   recognizedIDs = [];
+  pill= [];
+  pillData = [];
 
   
 
@@ -36,15 +38,15 @@ export class HomeComponent {
   ngOnInit() {
     
       this.loadData();
-      this.facialRecognize();
       this.getTempResults();
-  
+      this.getPillResults();
+      this.facialRecognize();
   }
 
   getTempResults()
   {
-    console.log("print user ## ",this.userSubject.value.username);
-    this.appService.getDeals(this.userSubject.value.username).subscribe(
+    console.log("print user for temperature## ",this.userSubject.value.username);
+    this.appService.getTemperature(this.userSubject.value.username).subscribe(
       response => {
         this.cardsForHandset = response.temparature;
         console.log(response.temparature);
@@ -57,6 +59,24 @@ export class HomeComponent {
       }
     );
   }
+
+  getPillResults()
+  {
+    console.log("print user for pill ## ",this.userSubject.value.username);
+    this.appService.getPill(this.userSubject.value.username).subscribe(
+      response => {
+        this.pillData = response.pill;
+        console.log("pill data"+response.pill);
+        this.loadData();
+        //this.notifierService.showNotification('Todays deals loaded successfully. Click on any deal!', 'OK', 'success');
+      },
+      error => {
+        // alert('There was an error in receiving data from server. Please come again later!');
+        this.notifierService.showNotification('There was an error in receiving data from server!', 'OK', 'error');
+      }
+    );
+  }
+
   async refreshTemperature(){
     console.log("inside refresh temp");
     let latestTemp = null;
@@ -79,13 +99,36 @@ export class HomeComponent {
     
   }
 
+  async refreshPill(){
+    console.log("inside refresh pill data");
+    let latestpill= null;
+     await this.appService.generatePillData(this.userSubject.value.username).toPromise()
+     .then(res => 
+     // latestTemp = JSON.stringify(res))
+     latestpill = res)
+     .catch(msg => console.log('Error: ' + msg.status + ' ' + msg.statusText));
+     console.log("latestTemp : "+latestpill);
+    this.appService.updatePillData(this.userSubject.value.username,latestpill).subscribe(
+      response => {
+        console.log(response.result);
+        this.getPillResults();
+      },
+
+      error => {
+        this.notifierService.showNotification('There was an error in receiving data from server!', 'OK', 'error');
+      }
+    );
+    
+  }
+
   async facialRecognize()
   {
+    console.log("inside facial recogni");
     console.log("print user ## ",this.userSubject.value.username);
     this.appService.generateIndentifiedPateints(this.userSubject.value.username).subscribe(
       response => {
-        this.recognizedIDs = response.id;
-        console.log(response.id);
+        this.recognizedIDs = response.identifiedPateintName;
+        console.log("ID"+this.recognizedIDs);
         this.loadData();
         //this.notifierService.showNotification('Todays deals loaded successfully. Click on any deal!', 'OK', 'success');
       },
@@ -101,7 +144,10 @@ export class HomeComponent {
   loadData() {
     this.cards =this.cardsForHandset;
     this.ids = this.recognizedIDs;
+    this.pill = this.pillData;
     console.log(this.cards);
+    console.log(this.ids);
+    console.log(this.pill);
   }
 
   public webcamImage: WebcamImage = null;
